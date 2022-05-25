e1fb7b7cf5475876f06ae04b56bc14b2bb51c69e
CalvinPierce <calvinpierce99@gmail.com>
2022-05-25 11:59:34 -0400

    NavBar App Created W/ Web Dev Simplified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./NavBar";
+import Pricing from "./pages/Pricing"
+import About from "./pages/About"
+import Home from "./pages/Pricing";
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
